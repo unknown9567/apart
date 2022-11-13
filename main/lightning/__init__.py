28fa4ff353013354ec55c7d719cf328c73c95b03
unknown9567 <someone6795@gmail.com>
2022-11-13 09:14:46 +0800

    update trainer

--- main/lightning/__init__.py
+++ main/lightning/__init__.py
@@ -1,4 +1,4 @@
 from .trainer import *
 from .cifar import CIFARModelBase
-from .imagenet import ImageNetModelBase
 from .tiny_imagenet import TinyImageNetModelBase
+from .imagenet import ImageNetModelBase
